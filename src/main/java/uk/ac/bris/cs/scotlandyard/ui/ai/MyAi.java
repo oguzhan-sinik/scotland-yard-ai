81824690d53bc78e60fdcd7b2e54f744f2410897
Oguzhan Sinik <ogzdlk06@gmail.com>
2026-04-11 23:12:37 +0100

    first-steps

--- src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/MyAi.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/MyAi.java
@@ -1,13 +1,13 @@
 package uk.ac.bris.cs.scotlandyard.ui.ai;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 import jakarta.annotation.Nonnull;
 import io.atlassian.fugue.Pair;
-import uk.ac.bris.cs.scotlandyard.model.Ai;
-import uk.ac.bris.cs.scotlandyard.model.Board;
-import uk.ac.bris.cs.scotlandyard.model.Move;
-import uk.ac.bris.cs.scotlandyard.model.Piece;
+import uk.ac.bris.cs.scotlandyard.model.*;
 
 public class MyAi implements Ai {
 
@@ -16,8 +16,6 @@ public class MyAi implements Ai {
 	@Nonnull @Override public Move pickMove(
 			@Nonnull Board board,
 			Pair<Long, TimeUnit> timeoutPair) {
-		// returns a random move, replace with your own implementation
-
 
 		/*
 
@@ -30,52 +28,91 @@ public class MyAi implements Ai {
 
 		 So when writing the scoring functions, we can first get possible places that MrX can go. And using Dijkstra's
 		 algorithm we can get shorthest ways that each detective can go those places. And select one node that
-		 each detective can't go very easily.
+		 farthest from all detectives. Before takign the medians, taking the square of distances should be put
+		 us to the best move.
 
 
 		(OPTIONAL) for detectives, a good move is one that will either capture or get close to MrX.
 		 */
 
 
-
-
 		// SCORING FUNCTION
 
-		System.out.println("Blue: " + board.getDetectiveLocation(Piece.Detective.BLUE).orElseThrow().toString());
-		System.out.println("Red: " + board.getDetectiveLocation(Piece.Detective.RED).orElseThrow().toString());
-		System.out.println("Green: " + board.getDetectiveLocation(Piece.Detective.GREEN).orElseThrow().toString());
-		System.out.println("White: " + board.getDetectiveLocation(Piece.Detective.WHITE).orElseThrow().toString());
-		System.out.println("Yellow: " + board.getDetectiveLocation(Piece.Detective.YELLOW).orElseThrow().toString());
+		boolean areWeMrX = board.getAvailableMoves().asList().get(0).commencedBy() == Piece.MrX.MRX;
 
-		var moves = board.getAvailableMoves().asList();
-
-		boolean areWeMrX = moves.get(0).commencedBy() == Piece.MrX.MRX;
-
-		for (var move : moves) {
-			int destination = move.accept(new Move.Visitor<Integer>() {
-				@Override
-				public Integer visit(Move.SingleMove m){
-					return m.destination;
-				}
-
-				@Override
-				public Integer visit(Move.DoubleMove m){
-					return m.destination2;
-				}
-			});
-
-			System.out.println("from " + move.source() + " to " + destination);
-
-		}
-
-		System.out.println("\n=========================================\n");
-
-		if (areWeMrX) {
-			return moves.get(new Random().nextInt(moves.size()));
-		} else {
-			return moves.get(new Random().nextInt(moves.size()));
-		}
-
+		return pickMove(board, areWeMrX);
 
 	}
+
+	@Nonnull public Integer stepsRequired(Board board, int from, int to){
+
+		// TODO Dijkstra algorithm for finding the shortest path
+		// We have a starting point and dest point. The score we want as a return is the shortest distance.
+
+
+
+		return new Random().nextInt(5);
+	}
+
+	@Nonnull public Integer nodeScore(Board board, int mrXLoc, List<Integer> detectiveLocs){
+		int totalScore = 0;
+
+		for (int detLoc : detectiveLocs) {
+			int dist = stepsRequired(board, detLoc, mrXLoc);
+			totalScore += dist;
+		}
+
+		return totalScore;
+	}
+
+	@Nonnull public Move pickMove(Board board, boolean isMrX){
+
+		var moves = board.getAvailableMoves().asList();
+		Move bestMove = moves.get(0);
+		int bestScore = 0;
+		int newScore = 0;
+
+		for (Move move : moves) {
+
+			//Board.GameState nextState = ((Board.GameState) board).advance(move);
+			//int newScore = minimaxAlg(nextState, isMrX, 4);
+
+			//System.out.println("\nnew score " + newScore + "best score ever " + bestScore);
+
+			if(newScore > bestScore){
+				bestScore = newScore;
+				bestMove = move;
+			}
+		}
+		return bestMove;
+	}
+
+	@Nonnull public Integer minimaxAlg(Board board, boolean isMrX, int depth, int mrXLoc, List<Integer> detectiveLocs){
+
+		List<Integer> scores = new ArrayList<>();
+		var moves = board.getAvailableMoves().asList();
+
+		if (depth == 0) {
+			int totalScore = 0;
+			for ( int detectiveLoc : detectiveLocs) {
+				int dist = stepsRequired(board, detectiveLoc, mrXLoc);
+				totalScore = dist * dist;
+			}
+			return  totalScore;
+		}
+
+		int bestScore = 0;
+
+		for (int adjacent : board.getSetup().graph.adjacentNodes(mrXLoc)){
+
+			if (detectiveLocs.contains(adjacent)) continue;
+
+			int score = minimaxAlg(board, isMrX, depth - 1, mrXLoc, detectiveLocs);
+			if (score > bestScore) bestScore = score;
+
+		}
+
+		return isMrX ? Collections.max(scores) : Collections.min(scores);
+	}
+
 }
